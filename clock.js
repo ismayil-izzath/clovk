0e08726fe39230fa0cab282f1069db05e7998926
User <ismayilizzath13@gmail.com>
2024-10-23 17:28:47 +0530

    clock

---
+++ clock.js
@@ -0,0 +1,26 @@
+// Date() - is a predefined class to access date and time of the system
+
+// 1.create an object for the class
+
+function getTime(){
+
+const time=new Date()
+console.log(time);
+
+const hour=time.getHours()
+console.log(hour);
+
+const min=time.getMinutes()
+console.log(min);
+
+const sec=time.getSeconds()
+console.log(sec);
+
+result.innerHTML=`${hour}:${min}:${sec} ${hour>12?'PM':'AM'}`
+
+setTimeout(()=>{
+getTime()
+},1000)
+}
+
+getTime()
